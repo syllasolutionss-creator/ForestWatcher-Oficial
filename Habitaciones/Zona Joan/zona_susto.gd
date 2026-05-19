80e1c50931b514e7acf512367f7e397e0249c66c
aritzlorente23 <aritz.lorente23@lacetania.cat>
2026-05-19 10:17:14 +0200

    yumescare etxo

--- Habitaciones/Zona Joan/zona_susto.gd
+++ Habitaciones/Zona Joan/zona_susto.gd
@@ -1,12 +1,12 @@
 extends Area3D
 
-@onready var imagen_monstruo = $UI_Jumpscare/ImagenMonstruo
+@onready var video_susto = $UI_Jumpscare/VideoSusto
 
 func _ready():
 
 	body_entered.connect(_on_body_entered)
 
-	imagen_monstruo.visible = false
+	video_susto.visible = false
 
 func _on_body_entered(body):
 
@@ -16,12 +16,12 @@ func _on_body_entered(body):
 
 		monitoring = false
 
-		imagen_monstruo.visible = true
+		video_susto.visible = true
 
 		print("¡Jumpscare!")
 
 		await get_tree().create_timer(1.5).timeout
 
-		imagen_monstruo.visible = false
+		video_susto.visible = false
 
 		queue_free()
